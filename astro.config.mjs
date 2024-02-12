caf355796005ca9cda19bf0215e6883deb98f0cc
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-02-12 21:35:54 +0100

    i18n translation support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import AstroIcon from "astro-icon";
 import Icons from 'unplugin-icons/vite';
-
 import partytown from "@astrojs/partytown";
+
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), AstroIcon(), partytown()],
+  integrations: [tailwind(), AstroIcon(), partytown(), metaTags()],
   vite: {
     plugins: [Icons({
       compiler: 'astro'
---
+++ src/i18n/ui.ts
@@ -0,0 +1,32 @@
+export const languages = {
+  en: "English",
+  it: "Italiano",
+};
+
+export const defaultLang = "en";
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    "common.Available": "Available",
+    "common.Unavailable": "Unavailable",
+    "nav.Docs": "Docs",
+    "nav.APIReference": "API Reference",
+    "splash.Download": "Download",
+    "splash.CloneSource": "Clone Source",
+    "footer.ChooseLanguage": "Choose Language",
+    "footer.ConnectWithUs": "Connect With Us",
+    "footer.OurTechStack": "Our Tech Stack",
+  },
+  it: {
+    "common.Available": "Disponibile",
+    "common.Unavailable": "Non Disponibile",
+    "nav.Docs": "Doumentazione",
+    "nav.APIReference": "Riferimenti API",
+    "splash.Download": "Scarica",
+    "splash.CloneSource": "Clona Codice",
+    "footer.ChooseLanguage": "Selezione Lingua",
+    "footer.ConnectWithUs": "Contattaci",
+    "footer.OurTechStack": "Il Nostro Tech Stack",
+  },
+} as const;
