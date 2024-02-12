1edec3184332d6b324c63529bd51cd42e59fffb9
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-02-12 15:42:08 +0100

    Added partytown to offset gtag workload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import { defineConfig } from "astro/config";
 import AstroIcon from "astro-icon";
 import Icons from 'unplugin-icons/vite';
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), AstroIcon()],
+  integrations: [tailwind(), AstroIcon(), partytown()],
   vite: {
-    plugins: [
-      Icons({
-        compiler: 'astro',
-      }),
-    ],
-  },
+    plugins: [Icons({
+      compiler: 'astro'
+    })]
+  }
 });
