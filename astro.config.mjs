a06e5ebec256ebf9f58d2cdb106b4dc17d30050e
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-02-13 10:34:48 +0100

    Added astro compress to minify assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,35 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import AstroIcon from "astro-icon";
-import Icons from 'unplugin-icons/vite';
+import Icons from "unplugin-icons/vite";
 import partytown from "@astrojs/partytown";
-
 import metaTags from "astro-meta-tags";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), AstroIcon(), partytown(), metaTags()],
+  integrations: [
+    tailwind(),
+    AstroIcon(),
+    partytown(),
+    metaTags(),
+    compress({
+      CSS: true,
+      HTML: true,
+      Image: true,
+      JavaScript: true,
+      SVG: true,
+    }),
+  ],
   vite: {
-    plugins: [Icons({
-      compiler: 'astro'
-    })]
+    plugins: [
+      Icons({
+        compiler: "astro",
+      }),
+    ],
   },
   compressHTML: true,
-  site: "https://rake-engine.netlify.app"
+  site: "https://rake-engine.netlify.app",
 });
